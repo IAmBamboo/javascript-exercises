f1f8d43973afa677dac29e87f6ddcfff759bf212
IAmBamboo <chimeipho@gmail.com>
2022-04-05 17:39:45 -0600

    completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(message, count) {
+    let newMessage = '';
+    if (count < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < count; i++) {
+            newMessage = newMessage+message;
+        }
+        return newMessage;
+    }
 };
 
 // Do not edit below this line
