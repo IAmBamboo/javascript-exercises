ceed097d3c5fabc02e302f35a7cc8df63b484c21
IAmBamboo <chimeipho@gmail.com>
2022-04-05 18:22:31 -0600

    finished exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(message) {
+    let tempArray = [];
+    let reverse = '';
+    for (let i = 0; i < (message.length*2); i++) {
+        if (i>=message.length) {
+            reverse += tempArray.pop();
+        } else {
+            tempArray.push(message.charAt(i));
+        }
+    }
+    //for (let i = 0; i < message.length; i++) {
+    //    reverse += tempArray.pop()
+    //}
+    return reverse;
 };
 
 // Do not edit below this line
