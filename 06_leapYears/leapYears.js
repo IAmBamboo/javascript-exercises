56c837f35fc606078207f75954d74b6d04697366
IAmBamboo <chimeipho@gmail.com>
2022-04-07 16:38:10 -0600

    finished exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //divisible by 4
+    //unless divisible by 100(unless divisible by 400)
+    if (year%4===0) {
+        if ((year%100!==0)||((year%100===0)&&(year%400===0))) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
