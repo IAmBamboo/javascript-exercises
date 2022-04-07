03d185d0f45bcc7807f899a513cdf645377bc546
IAmBamboo <chimeipho@gmail.com>
2022-04-07 16:03:30 -0600

    finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let finalSum = 0;
+    if (((typeof(a)=='number')&&(typeof(b)=='number'))&&(a>0&&b>0)) {
+        let endRange = b > a ? b : a;
+        for (let i = 0; i < endRange; i++) {
+            finalSum += i+1;
+        }
+        console.log(finalSum);
+        return finalSum;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
