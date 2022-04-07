25c6a540a853dc4f9d560aae13ab5945f8de9803
IAmBamboo <chimeipho@gmail.com>
2022-04-07 16:49:45 -0600

    finished exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celcius = (temp-32)*(5/9)
+  let tempRounded = Math.round(celcius * 10) / 10
+  return tempRounded;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let fahrenheit = (temp*(9/5))+32
+  let tempRounded = Math.round(fahrenheit * 10) / 10
+  return tempRounded;
 };
 
 // Do not edit below this line
